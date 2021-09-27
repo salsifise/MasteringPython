7ae8f6539b207a6ccd571ea685d78ab97e7f0d43
salsifise <36170689+salsifise@users.noreply.github.com>
2021-09-27 13:11:59 +0200

    task 01 week 03

---
+++ third.py
@@ -0,0 +1,7 @@
+# # WELCOME TO PYTHON # #
+# Programming Python. WEEK 03
+# 019 --> 020
+# TASK 001 - print all types of numbers
+print(type(10)) # integer
+print(type(10.5)) # float
+print(type(5+2j)) # complex
