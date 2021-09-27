b809d1a7e40bc4330ad962b9f79155553bb51c53
salsifise <36170689+salsifise@users.noreply.github.com>
2021-09-27 08:33:23 +0200

    until task 013 (formatting)

--- second.py
+++ second.py
@@ -44,10 +44,14 @@ num = "1500"
 print(num.zfill(4))
 #--------------------------------------------------
 # TASK 007 - putting chars before string
+# Method 01:
 name_one = "Younes"
 name_two = "Younes_Elzero"
 print(name_one.center(34,"@").rstrip("@"))
 print(name_two.center(28,"@").rstrip("@"))
+# Method 02:
+print(name_one.rjust(20, "@"))
+print(name_two.rjust(20, "@"))
 #--------------------------------------------------
 # TASK 008 - capitalize letters Aa
 name_1 = "YOunes" # OUTPUT >>  yoUNES
@@ -72,6 +76,12 @@ MSG = "I <3 Python and Although <3 Elzero Web School"
 print(MSG.replace("<3", "Love", 1))
 #--------------------------------------------------
 # TASK 013 - print name, age(int), country by Format ""f
+# Method 01: Old way.
 namee = "Younes"
 agge = 23
 countryy = "Algeria"
+print("My name is %s, And my Age is %d, And my Country is %s" %(namee, agge, countryy))
+# Method 01: New way.
+print("My name is {:s}, And my Age is {:d}, And my Country is {:s}".format(namee, agge, countryy))
+# Method 03: New way. Like in Java script
+print(f"My name is {namee}, And my Age is {agge}, And my Country is {countryy}")
