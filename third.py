cb04ec49737df151fa1fb6622429f9c3207980f9
[Younes Lachemat] <salsifise@gmail.com>
2021-10-07 13:10:22 +0200

    untill asseignmnets 022

--- third.py
+++ third.py
@@ -1,7 +1,35 @@
 # # WELCOME TO PYTHON # #
 # Programming Python. WEEK 03
 # 019 --> 020
-# TASK 001 - print all types of numbers
+# TASK 001 - Types of numbers
 print(type(10)) # integer
 print(type(10.5)) # float
 print(type(5+2j)) # complex
+print(type(5 == 5)) # boolean
+#-------------------------------------------------
+# Task 002 - COMPLEX Number
+a = 1+2j
+print(a.imag)
+print(a.real)
+#-------------------------------------------------
+# Task 003 - transfer integer number to real number
+num = 10
+numm = float(num)
+print('%.10f'%numm)
+#-------------------------------------------------
+# Task 004 - transfer real number to integer number
+num = 159.650
+numm = int(num)
+print(numm)
+print(type(numm))
+#-------------------------------------------------
+# Task 005 - putting the true operator 
+print(100 - 115 == -15)
+print(50 * 30 == 1500)
+print(21 % 4 == 1)
+print(110 / 11 == 10)
+print(97 // 20 == 4)
+#-------------------------------------------------
+
+#-------------------------------------------------
+# 021 --> 023 
